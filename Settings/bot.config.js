7d8d25b28441b4081e375e7fb9f6c89db7b97e89
at00m <79448212+justat00m@users.noreply.github.com>
2022-12-20 20:11:55 +0300

    Update bot.config.js

--- Settings/bot.config.js
+++ Settings/bot.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     bot: {
-        TOKEN : "MTA1MTE5NjA0NDczNTY5Njk2Ng.G4h57F.7PfLi49Uo-frKZRa3d6P2XWMAWtwi103wqsJ14",
-        ID: "1051196044735696966"
+        TOKEN : "",
+        ID: ""
     },
     database: {
-        URL: "mongodb+srv://MalFiirliss:dOu78DywAROSlg0K@malfearless.xo6xz.mongodb.net/?retryWrites=true&w=majority"
+        URL: ""
     },
 
     supportServer: {
-        ID: "841254502485196811"
+        ID: ""
     }
 };
